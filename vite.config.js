c9a32d22bbcf1321b312d0ee30a4357c95d9cb93
rafael difini <rafaeldifini@live.com>
2025-09-28 18:04:50 -0300

    correçoes

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,12 @@ import tailwindcss from "@tailwindcss/vite"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
-    base: '/rubinot-tools/',
   },
+  base: "/rubinot-tools/", 
 })
